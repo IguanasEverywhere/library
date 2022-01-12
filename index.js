2526830b305dac46a7ccd63317d944af87854663
Scott Schwab <schwabpiano@gmail.com>
2022-01-12 11:38:26 -0600

    Further CSS styling

--- index.js
+++ index.js
@@ -19,10 +19,30 @@ function addBookToLibrary(book) {
 function displayBooks(book) {
   let itemCard = document.createElement("div");
   itemCard.className = "card";
-  itemCard.textContent = book.title + book.author + book.pages + book.read;
+
+  let itemList = document.createElement("ul");
+
   document.getElementById("cards-body").appendChild(itemCard);
+  itemCard.appendChild(itemList);
+
+  let itemTitle = document.createElement("li");
+  itemTitle.textContent=book.title;
+  itemList.appendChild(itemTitle);
+
+  let itemAuthor = document.createElement("li");
+  itemAuthor.textContent=book.author;
+  itemList.appendChild(itemAuthor);
+
+  let itemPages = document.createElement("li");
+  itemPages.textContent=book.pages;
+  itemList.appendChild(itemPages);  
+
+  let itemRead = document.createElement("li");
+  itemRead.textContent=book.read;
+  itemList.appendChild(itemRead);
 }
 
+const bookForm = document.getElementById("bookForm");
 
 const submitBtn = document.getElementById("submitBtn");
 submitBtn.addEventListener("click", () => {
@@ -33,6 +53,9 @@ submitBtn.addEventListener("click", () => {
 
   let addedBook = new Book(addedTitle, addedAuthor, addedPages, addedRead);
   addBookToLibrary(addedBook);
+
+  bookForm.reset();
+
 });
 
 
