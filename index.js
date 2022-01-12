3fcab76cdc67c9e096209f30e133c0706ef201bb
Scott Schwab <schwabpiano@gmail.com>
2022-01-12 09:37:48 -0600

    Move submit button outside of form, get rid of placeholders.

--- index.js
+++ index.js
@@ -1,47 +1,54 @@
 let myLibrary = [];
 
 function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-  
-    this.info = function() {
-      return (this.title + this.author + this.pages + this.read);
-    }
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+
+  this.info = function () {
+    return (this.title + this.author + this.pages + this.read);
   }
+}
 
-  function addBookToLibrary(book) {
-      myLibrary.push(book);
-  }
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+  console.log(book);
 
-  function formSubmit() {
-      let addedTitle = document.getElementById("bookTitle").value;
-      let addedAuthor = document.getElementById("bookAuthor").value;
-      let addedPages = document.getElementById("bookPages").value;
-      let addedRead = document.getElementById("bookRead").value;
+  displayBooks();
+}
 
-      addBookToLibrary(addedTitle, addedAuthor, addedPages, addedRead);
-
-      let addedBook = new Book(addedTitle, addedAuthor, addedPages, addedRead);
-   alert(addedBook.addedTitle); // this doesn't work
-   alert(addedTitle); // this does
-   alert(typeof(addedTitle)); // yes it's a string
-      
-  }
-
-  let book1 = new Book("Deep Work", "Cal Newport", 296, true);
-  let book2 = new Book("A Walk In the Woods", "Bill Bryson", 275, true);
-
-  addBookToLibrary(book1);
-  addBookToLibrary(book2);
-
+function displayBooks() {
   myLibrary.forEach(entry => {
     let itemCard = document.createElement("div");
     itemCard.className = "card";
     itemCard.textContent = entry.title + entry.author + entry.pages + entry.read;
     document.getElementById("cards-body").appendChild(itemCard);
-  }); 
+  });
+}
 
 
-  console.log(myLibrary);
+
+
+const submitBtn = document.getElementById("submitBtn");
+submitBtn.addEventListener("click", () => {
+  const addedTitle = document.getElementById("bookTitle").value;
+  const addedAuthor = document.getElementById("bookAuthor").value;
+  const addedPages = document.getElementById("bookPages").value;
+  const addedRead = document.getElementById("bookRead").value;
+
+  let addedBook = new Book(addedTitle, addedAuthor, addedPages, addedRead);
+  addBookToLibrary(addedBook);
+});
+
+
+
+// let book1 = new Book("Deep Work", "Cal Newport", 296, true);
+// let book2 = new Book("A Walk In the Woods", "Bill Bryson", 275, true);
+
+// addBookToLibrary(book1);
+// addBookToLibrary(book2);
+
+
+
+console.log(myLibrary);
